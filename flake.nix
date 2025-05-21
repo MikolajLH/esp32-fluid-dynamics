1bf9d5f7481978d56152ecb6463b5715821c0bb2
Rellikeht <michaelhemperek@gmail.com>
2025-05-21 15:58:20 +0200

    add python to environment

--- flake.nix
+++ flake.nix
@@ -18,11 +18,33 @@
 
         packages = with pkgs; [
           arduino-ide
+
+          uv
+          python313Full
         ];
+
+        libs = with pkgs; [
+          zlib
+          stdenv.cc.cc.lib
+          glib
+
+          # for matplotlib to fucking work
+          fontconfig
+          xorg.libX11
+          libxkbcommon
+          freetype
+          dbus
+        ];
+        ld_path = "${pkgs.lib.makeLibraryPath libs}";
       in {
         devShells = {
           default = envs.arduino.overrideAttrs (
-            oldAttrs: {buildInputs = oldAttrs.buildInputs ++ packages;}
+            oldAttrs: {
+              buildInputs = oldAttrs.buildInputs ++ packages ++ libs;
+              shellHook = ''
+                export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${ld_path}"
+              '';
+            }
           );
         };
       }
