882d6ebef59b052de6868edfd4bde8d0800ae108
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-05-21 14:47:10 +0200

    added external forces

--- lbm.py
+++ lbm.py
@@ -3,12 +3,13 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 # --- Parameters ---
-nx, ny = 100, 50  # Grid size
+nx, ny = 64, 32  # Grid size
 tau = 0.6  # Relaxation time
 omega = 1.0 / tau  # Relaxation parameter
 inflow_velocity = 0.1
 UPDATES = 10
-STEPS = 500000
+STEPS = 100
+
 
 # --- D2Q9 Lattice ---
 directions = np.array(
@@ -47,20 +48,42 @@ for i in range(9):
 # --- Matplotlib Setup ---
 fig, ax = plt.subplots()
 im = ax.imshow(
-    np.sqrt(ux**2 + uy**2).T, cmap="plasma", origin="lower", vmin=0, vmax=0.15
+    #np.sqrt(ux**2 + uy**2).T,
+    np.sum(f, axis=0).T,
+    cmap="plasma", origin="lower", 
 )
 plt.colorbar(im, ax=ax)
 ax.set_title("LBM Fluid Simulation (Velocity Magnitude)")
 
 
 # --- Main Simulation Step ---
-def lbm_step():
+def lbm_step(s):
     global f, rho, ux, uy
 
     # Collision
     for i in range(9):
         feq = equilibrium(i, rho, ux, uy)
         f[i] += -omega * (f[i] - feq)
+
+    rx, ry = 0, 0
+    if s % 100 == 0:
+        rx = (np.random.rand() * 2 - 1) * 1.e-1
+        ry = (np.random.rand() * 2 - 1) * 1.e-1
+            
+    force_x = np.sin(2 * np.pi * s/(UPDATES * STEPS)) * -1e-2 + rx
+    force_y = -1e-2 + ry # gravity downward
+
+    cs2 = 1.0 / 3.0
+    for i in range(9):
+        ei = directions[i]
+        eu = ei[0]*ux + ei[1]*uy
+        force_term = (
+            ((ei[0] - ux) + ei[0]*eu / cs2) * force_x +
+            ((ei[1] - uy) + ei[1]*eu / cs2) * force_y
+        )
+
+        Fi = weights[i] * (1 - 0.5 * omega) * force_term / cs2 * rho
+        f[i] += Fi
 
     # Streaming
     for i in range(9):
@@ -88,10 +111,12 @@ def lbm_step():
 
 # --- Animation Function ---
 def update(frame):
-    for _ in range(UPDATES):  # More LBM steps per frame
-        lbm_step()
+    for i in range(UPDATES):  # More LBM steps per frame
+        lbm_step(frame * UPDATES + i)
     speed = np.sqrt(ux**2 + uy**2)
-    im.set_array(speed.T)
+    denst = np.sum(f, axis=0)
+    #im.set_array(speed.T)
+    im.set_array(denst.T)
     ax.set_title(f"Step {frame * UPDATES}")
     return [im]
 
@@ -99,5 +124,5 @@ def update(frame):
 ani = animation.FuncAnimation(
     fig, update, frames=STEPS, interval=1, blit=False
 )
-# ani.save("animation.gif", writer="imagemagick", fps=15)
+#ani.save("animation.gif", writer="imagemagick", fps=15)
 plt.show()
